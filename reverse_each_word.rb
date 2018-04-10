394eb6b972dd066661967d79b9f3d923b394240f
Ayana Bando <ayanaflax@yahoo.com>
2018-04-10 16:10:20 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 require 'pry'
 def reverse_each_word(sentence1)
   array = []
-  array << sentence1.split("")
+  array << sentence1.split(" ")
   array.each do |sentence|
     sentence.reverse
   end
