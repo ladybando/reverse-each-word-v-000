d560e015b67e88deab52fc6b57a6c51d94aaf799
Ayana Bando <ayanaflax@yahoo.com>
2018-04-10 16:12:47 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,9 +3,9 @@ def reverse_each_word(sentence1)
   array = []
   array << sentence1.split(" ")
   array.each do |sentence|
-    sentence.reverse
-    binding.pry
+  sentence.reverse
   end
+  binding.pry
     return array.join(" ")
   end
 end
